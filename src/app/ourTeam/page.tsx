d1c3ba058038f61a3799d0b4e7a5537c3956ef59
Ahdan24 <Auvailable@gmail.com>
2024-11-17 21:39:08 +0700

    ready to deploy

--- src/app/ourTeam/page.tsx
+++ src/app/ourTeam/page.tsx
@@ -27,13 +27,13 @@ const OurTeam = () => {
   if (error) return <p>{error}</p>;
 
   return (
-    <section className="container mx-auto p-4">
+    <section className="container mx-auto p-4 bg-gray-800 px-10">
       <h1 className="text-3xl font-bold text-center mb-8"> Meet Our Team</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {teamMembers.map((member, index) => (
           <div
             key={index}
-            className="bg-white shadow-md rounded-lg p-4 text-center"
+            className="bg-gray-800 shadow-md text-white border-white border-2 rounded-lg p-4 text-center"
           >
             <img
               src={member.picture.large}
